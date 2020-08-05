fa7fed1bab6eb742ed104b613bec74b28d3909b0
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-06 00:05:52 +0900

    An Important Note about Route Observables: Unsubscribing Observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-
+  paramsSubscription: Subscription
   // To avoid confusion: The ActivatedRoute object we injected will give us access to the id passed in the URL => Selected User
   constructor(private route:ActivatedRoute) { }
 
@@ -27,13 +28,17 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
 
-    this.route.params
+    this.paramsSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
   }
 
 }
