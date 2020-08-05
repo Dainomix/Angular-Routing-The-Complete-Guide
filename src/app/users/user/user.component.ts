ccb195c0c48b09ffde13c843b99a7efce6bbb84e
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-05 23:58:23 +0900

    Fetching Route Parameters Reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -12,12 +12,28 @@ export class UserComponent implements OnInit {
   // To avoid confusion: The ActivatedRoute object we injected will give us access to the id passed in the URL => Selected User
   constructor(private route:ActivatedRoute) { }
 
+  /**
+   * We might simply use the snapshot
+   * because if i know my component will 100% of the time be recreated when it is reached,
+   * because there's no other way of reaching it, there is no way of reaching it whilist being on that component
+   * if i know that, i don't need to subscribe.
+   * In all other cases, make sure to use this approach to get informed about any changes in my route parameters.
+   */
+
   ngOnInit() {
     this.user =  {
       // access to properties here which i defined in my route parameters. which is 'users/:id/:name' in appRoutes: Routes
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
