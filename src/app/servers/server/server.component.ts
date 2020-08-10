237624d1a1cf8b618b69c31e73e5fd1dace53361
Kim Hyeoncheol <effectmix@gmail.com>
2020-08-10 23:23:26 +0900

    Add comments about between preserve and merge in queryParamsHandling

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -26,6 +26,20 @@ export class ServerComponent implements OnInit {
     )
   }
 
+  /**
+   * To preserve that information,
+   * we get a simple way of doing so. In the server component, where we navigate, we can pass another property
+   * or add another property to his javascript object we use to configure our navigation
+   * and here, we get the query params handling property
+   * Now query params handling takes a string as a value and this could be merge, to merge our old query params
+   * with any new we might add here. Now we don't andd any new ones,
+   * So we can simply choose preserve instead and preserve which will overwrite the default behavior
+   * which is simply drop them and make sure that the old ones are kept.
+   * 
+   * Now if we were to add new ones here, the old ones would actually overwrite the new ones,
+   * so we should use merge in this case.
+   * but since we don't add new ones, preserve should make sure that we don't lose them
+   */
   onEdit() {
    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'}); 
   }
